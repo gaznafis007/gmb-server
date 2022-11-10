c1f917d4902d45738e3a6937f32d66a2b319cce7
gazinafis007 <gazinafisrafi.gnr@gmail.com>
2022-11-10 20:48:05 +0600

    vercel and environment variable

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 require("dotenv").config();
 const app = express();
@@ -8,6 +9,19 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("server running");
+});
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9isdf3i.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
 });
 
 app.listen(port, () => {
